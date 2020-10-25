cef8dfd91eb21780f79b42e3353530a636d73d06
HollandPleskac <hollandpleskac@gmail.com>
2020-10-25 14:39:34 -0700

    small circle

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -355,7 +355,7 @@ class LiveMap extends StatelessWidget {
                             encounter.coords['lat'],
                             encounter.coords['lng'],
                           ),
-                          radius: 500000000000000,
+                          radius: 5000000,
                           fillColor: Colors.red.withOpacity(0.5),
                           strokeWidth: 0,
                           visible: true);
